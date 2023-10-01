b7441e846a98d4ce2fc686aeb131baa8bb529e2b
Mdominykas <marmadominykas@gmail.com>
2023-10-01 21:51:10 +0300

    templated-hungarian

--- code/graphs/hungarian_algorithm.cpp
+++ code/graphs/hungarian_algorithm.cpp
@@ -1,9 +1,10 @@
-int findMinAssignment(vector<vector<int> > C)
+template<typename T>
+T findMinAssignment(vector<vector<T> > C)
 {
   int n = C.size(), m = C[0].size();
   // cout << "n, m = " << n << ", " << m << endl;
   assert(n <= m);
-  int a[n + 1][m + 1];
+  T a[n + 1][m + 1];
   for(int i = 1; i <= n; i++)
   {
     for(int j = 1; j <= m; j++)
@@ -11,25 +12,26 @@ int findMinAssignment(vector<vector<int> > C)
       a[i][j] = C[i - 1][j - 1];
     }
   }
-  const int INF = 1e9;
 
-  vector<int> u(n + 1, 0), v(m + 1, 0), p(m + 1), way(m + 1);
+  vector<T> u(n + 1, 0), v(m + 1, 0);
+  vector<int> p(m + 1), way(m + 1);
   // p[i] - corresponding row for column i in matching
   for(int i = 1; i <= n; i++)
   {
     p[0] = i;
     int j0 = 0; // free column
-    vector<int> minv(m + 1, INF);
+    vector<T> minv(m + 1, INF);
     vector<bool> used(m + 1, false); // jau panaudotas paieskoje
     do
     {
       used[j0] = true;
-      int i0 = p[j0], delta = INF, j1;
+      int i0 = p[j0], j1;
+      T delta = INF;
       for(int j = 1; j <= m; j++)
       {
         if(!used[j])
         {
-          int cur = a[i0][j] - u[i0] - v[j];
+          T cur = a[i0][j] - u[i0] - v[j];
           if(cur < minv[j])
           {
             minv[j] = cur;
@@ -67,7 +69,7 @@ int findMinAssignment(vector<vector<int> > C)
     while(j0);
   }
   
-  int cost = -v[0];
+  T cost = -v[0];
   return cost;
 
 }
